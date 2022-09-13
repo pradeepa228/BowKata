138197693001181c741406a0115b53ee2a8c04d7
pradeepa228 <111800846+pradeepa228@users.noreply.github.com>
2022-09-13 23:06:05 +0100

    First Passing Test case

---
+++ Bowling.Tests/BowlingScoreCalculatorTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace Bowling.Tests
+{
+    public class BowlingScoreCalculatorTest
+    {
+        private BowlingScoreCalculator _bowlingScoreCalculator;
+        [SetUp]
+        public void Setup()
+        {
+            _bowlingScoreCalculator = new BowlingScoreCalculator();
+        }
+
+        [Test]
+        public void Calculate_Sum_Of_KnowdownPins_Should_return_Sum()
+        {
+            string inputString = "12 12 12 12 12 12 12 12 12 12";
+            int expectedResult = 30;
+            _bowlingScoreCalculator.calculate_total(inputString).Should().Be(expectedResult);
+        }
+    }
+}
---
+++ Bowling/BowlingScoreCalculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bowling
+{
+    public class BowlingScoreCalculator
+    {
+        public BowlingScoreCalculator()
+        {
+
+        }
+        public int calculate_total(string inputString)
+        {
+            int sum = 0;
+            string[] frames = inputString.Split(' ');
+            foreach(var word in frames)
+            {
+                sum += word.Sum(c => c - '0');
+                Console.WriteLine(word + " " + sum);
+            }
+            return sum;
+        }
+    }
+}
