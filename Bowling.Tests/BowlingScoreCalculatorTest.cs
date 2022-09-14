7d109e872965e46484a7b1f03b158bb57b082a58
pradeepa228 <111800846+pradeepa228@users.noreply.github.com>
2022-09-14 20:56:42 +0100

    Few test cases of Strike and Spare

--- Bowling.Tests/BowlingScoreCalculatorTest.cs
+++ Bowling.Tests/BowlingScoreCalculatorTest.cs
@@ -17,11 +17,114 @@ namespace Bowling.Tests
         }
 
         [Test]
-        public void Calculate_Sum_Of_KnowdownPins_Should_return_Sum()
+        public void Calculate_Sum_Of_KnowdownPins_Should_return_30()
         {
-            string inputString = "12 12 12 12 12 12 12 12 12 12";
-            int expectedResult = 30;
+            string inputString = "22 22 22 22 22 22 22 22 22 22";
+            int expectedResult = 40;
             _bowlingScoreCalculator.calculate_total(inputString).Should().Be(expectedResult);
         }
+
+        [Test]
+        public void Calculate_Sum_Of_KnowdownPins_Should_return_90()
+        {
+            string inputString = "9- 9- 9- 9- 9- 9- 9- 9- 9- 9-";
+            int expectedResult = 90;
+            _bowlingScoreCalculator.calculate_total(inputString).Should().Be(expectedResult);
+        }
+
+        [Test]
+        public void Calculate_Sum_Of_KnowdownPins_Should_return_43()
+        {
+            string inputString = "8/ 81 11 11 11 11 11 11 11 11";
+            int expectedResult = 43;
+            _bowlingScoreCalculator.calculate_total(inputString).Should().Be(expectedResult);
+        }
+
+
+        [Test]
+        public void Calculate_Sum_Of_KnowdownPins_Should_return_52()
+        {
+            string inputString = "8/ 81 11 11 5/ 11 11 11 11 11";
+            int expectedResult = 52;
+            _bowlingScoreCalculator.calculate_total(inputString).Should().Be(expectedResult);
+        }
+
+        [Test]
+        public void Calculate_Sum_Of_KnowdownPins_Should_return_61()
+        {
+            string inputString = "51 X 32 32 32 32 32 32 32 32";
+            int expectedResult = 61;
+            _bowlingScoreCalculator.calculate_total(inputString).Should().Be(expectedResult);
+        }
+
+
+        [Test]
+        public void Calculate_Sum_Of_KnowdownPins_Should_return_71()
+        {
+            string inputString = "51 X 32 32 X 32 32 32 32 32";
+            int expectedResult = 71;
+            _bowlingScoreCalculator.calculate_total(inputString).Should().Be(expectedResult);
+        }
+
+
+        [Test]
+        public void Calculate_Sum_Of_KnowdownPins_Should_return_81()
+        {
+            string inputString = "51 X 32 32 X 32 32 32 32 3/ 5";
+            int expectedResult = 81;
+            _bowlingScoreCalculator.calculate_total(inputString).Should().Be(expectedResult);
+        }
+
+        [Test]
+        public void Calculate_Sum_Of_KnowdownPins_Should_return_83()
+        {
+            string inputString = "51 X 32 32 X 32 32 32 32 X 52";
+            int expectedResult = 83;
+            _bowlingScoreCalculator.calculate_total(inputString).Should().Be(expectedResult);
+        }
+        [Test]
+        public void Calculate_Sum_Of_KnowdownPins_Should_return_78()
+        {
+            string inputString = "X X 23 45 12 12 23 43 21 33";
+            int expectedResult = 78;
+            _bowlingScoreCalculator.calculate_total(inputString).Should().Be(expectedResult);
+        }
+        [Test]
+        public void Calculate_Sum_Of_KnowdownPins_Should_return_252()
+        {
+            string inputString = "X X X X X X X X X 23";
+            int expectedResult = 252;
+            _bowlingScoreCalculator.calculate_total(inputString).Should().Be(expectedResult);
+        }
+        [Test]
+        public void Calculate_Sum_Of_KnowdownPins_Should_return_127()
+        {
+            string inputString = "X X X 23 23 23 23 23 23 X X X";
+            int expectedResult = 127;
+            _bowlingScoreCalculator.calculate_total(inputString).Should().Be(expectedResult);
+        }
+        [Test]
+        public void Calculate_Sum_Of_KnowdownPins_Should_return_300()
+        {
+            string inputString = "X X X X X X X X X X X X";
+            int expectedResult = 300;
+            _bowlingScoreCalculator.calculate_total(inputString).Should().Be(expectedResult);
+        }
+        /*
+        [Test]
+        public void Calculate_Sum_Of_KnowdownPins_Should_return_150()
+        {
+            string inputString = "5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5";
+            int expectedResult = 150;
+            _bowlingScoreCalculator.calculate_total(inputString).Should().Be(expectedResult);
+        }*/
+        /*
+        [Test]
+        public void Calculate_Sum_Of_KnowdownPins_Should_return_300()
+        {
+            string inputString = "X X X X X X X X X X";
+            int expectedResult = 300;
+            _bowlingScoreCalculator.calculate_total(inputString).Should().Be(expectedResult);
+        }*/
     }
-}
+    }
--- Bowling/BowlingScoreCalculator.cs
+++ Bowling/BowlingScoreCalculator.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,11 +16,79 @@ namespace Bowling
         public int calculate_total(string inputString)
         {
             int sum = 0;
+            int iterartor = 0;
+            inputString = inputString.Replace('-', '0');
+            bool checkSpare = false;
+            bool checkStrike = false;
+            bool contiStrike = false;
+            
+
             string[] frames = inputString.Split(' ');
-            foreach(var word in frames)
+            foreach (var tries in frames)
             {
-                sum += word.Sum(c => c - '0');
-                Console.WriteLine(word + " " + sum);
+                iterartor++;
+                Char spareValue = '/';
+                Char strikeValue = 'X';
+
+
+                if (tries.Contains(spareValue))
+                {
+                    //Console.WriteLine("Contains Spare");
+                    sum = sum + 10;
+                    checkSpare = true;
+                } else if (tries.Contains(strikeValue)) {
+                    Console.WriteLine(iterartor + "iterartor");
+                    sum = sum + 10;
+                    if (checkStrike == true)
+                    {
+                        if (contiStrike == true && iterartor < 10)
+                        {
+                            sum = sum + 20;
+                        }
+                        else
+                        {
+                            sum = sum + 10;
+                            contiStrike = true;
+                        }
+                    }
+                    if (iterartor < 10)
+                    {
+                        checkStrike = true;
+                    }
+                    
+                } else {
+                    int number = Int32.Parse(tries);
+                    //Console.WriteLine("converted number:" + number);
+                    while (number > 0)
+                    {
+                        if (checkStrike == true)
+                        {
+                            // Console.WriteLine("Strike" + checkStrike); 
+                            if(contiStrike == true)
+                            {
+                                sum += number / 10;
+                                contiStrike = false;
+                            }
+                            sum += number / 10;
+                            sum += number % 10;
+                            checkStrike = false;
+                            
+                        }
+
+                        sum += number % 10;
+                        number /= 10;
+
+                        if (checkSpare == true)
+                        {
+                            Console.WriteLine("Spare" + checkSpare); 
+                            sum = sum + number;
+                            checkSpare = false;
+                            
+                        }
+                    }
+                    
+                } 
+                Console.WriteLine(tries + " " + sum);
             }
             return sum;
         }
